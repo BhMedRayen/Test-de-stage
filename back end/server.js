c23d0fb768bbebd41b688dac49d05de6d39534fa
BhMedRayen <rayenmohamed2310@gmail.com>
2023-12-25 17:06:53 +0100

    upload file

--- back end/server.js
+++ back end/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 
 app.use('/menuItems', menuItemRoutes);
+app.use((req, res, next) => {
+    console.log('Request Body:', req.body);
+    next();
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- back end/src/controller/MenuItemController.js
+++ back end/src/controller/MenuItemController.js
@@ -1,16 +1,38 @@
-
 const MenuItem = require('../models/menuItem'); 
+const multer = require('multer');
+
+let filename = ''; 
+
+const mystorage = multer.diskStorage({
+    destination: 'upload',
+    filename: (req, file, redirect) => {
+        let date = Date.now();
+        let fn = date + '.' + file.mimetype.split('/')[1];
+        redirect(null, fn);
+        filename = fn;
+    }
+});
+
+
+const upload = multer({ storage: mystorage });
+
+const uploadMiddleware = upload.any('image');
 
 exports.createMenuItem = async (req, res) => {
     try {
-        console.log('Request Body:', req.body); 
-        const menuItem = new MenuItem(req.body);
-        await menuItem.save();
-        res.status(201).json(menuItem);
+        uploadMiddleware(req, res, () => {
+            console.log('Request Body:', req.body); 
+            const menuItem = new MenuItem(req.body);
+            menuItem.image = filename;
+            menuItem.save();
+            filename = '';
+            res.status(201).json(menuItem);
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
+
 
 
 exports.getAllMenuItems = async (req, res) => {
