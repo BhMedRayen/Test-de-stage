402fd784524d363334be029555376c56c90c918a
BhMedRayen <rayenmohamed2310@gmail.com>
2023-12-25 18:08:37 +0100

    tester bootstrap

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,21 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container mt-5">
+      {/* En-tête de la page */}
+      <header className="text-center">
+        <h1>Ma Page Web Bootstrap</h1>
       </header>
+
+      {/* Contenu principal */}
+      <div className="row mt-3">
+        <div className="col-md-6">
+          {/* Formulaire de connexion */}
+          <form>
+            <div className="form-group">
+              <label htmlFor="username">Nom d'utilisateur :</label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="Entrez votre nom d'utilisateur"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Mot de passe :</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Entrez votre mot de passe"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Se connecter
+            </button>
+          </form>
+        </div>
+        <div className="col-md-6">
+          {/* Contenu supplémentaire */}
+          <p>Bienvenue sur ma page web Bootstrap. Ceci est un exemple de mise en page simple.</p>
+        </div>
+      </div>
     </div>
   );
 }
